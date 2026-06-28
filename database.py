c2fda91107cbd175ee1a546f5b6f81cf906abd66
BYEL21 <aabaltazar21@gmail.com>
2026-06-28 23:25:24 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,22 @@
 from config import supabase
 
 
-def get_student(student_id):
+def get_student(student_id, student_name):
 
+    response = (
+        supabase
+        .table("students")
+        .select("*")
+        .eq("student_id", student_id)
+        .eq("student_name", student_name)
+        .execute()
+    )
+
+    if response.data:
+        return response.data[0]
+
+    return None
+    
     response = (
         supabase
         .table("students")
