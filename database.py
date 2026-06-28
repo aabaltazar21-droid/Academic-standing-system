96988c471f4cc614178712e1b75133bfc67e4155
BYEL21 <aabaltazar21@gmail.com>
2026-06-28 23:07:03 +0800

    Update database.py

--- database.py
+++ database.py
@@ -1,29 +1,14 @@
 from config import supabase
 
 
-# ==========================================================
-# Student Functions
-# ==========================================================
+def test_connection():
 
-def create_student(student_id, student_name):
-
-    return (
-        supabase.table("students")
-        .insert(
-            {
-                "student_id": student_id,
-                "student_name": student_name,
-            }
-        )
-        .execute()
-    )
-
-
-def get_student(student_id):
-
-    return (
-        supabase.table("students")
+    response = (
+        supabase
+        .table("students")
         .select("*")
-        .eq("student_id", student_id)
+        .limit(1)
         .execute()
     )
+
+    return response
